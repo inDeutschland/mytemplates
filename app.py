e9b70683bc6d4d5d848ae2f072a5e1e54fd047b7
TamerOnLine <tamer@tameronline.com>
2025-06-21 01:49:19 +0200

    02

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Welcome to the API!"
+    return render_template("index.j2")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4444, debug=True)
